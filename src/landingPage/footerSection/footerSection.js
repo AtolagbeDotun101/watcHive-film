7c4c7c0484e9ac5039e77cb5abba66331508d24a
Atolagbe Dotun <dotundotun20@gmail.com>
2023-02-24 10:44:50 +0100

    landingpage

---
+++ src/landingPage/footerSection/footerSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footerSection.css'
+
+const footerSection = () => {
+  return (
+    <div id='footer'>
+        footerSection
+    </div>
+  )
+}
+
+export default footerSection
---
+++ src/landingPage/thirdSection/thirdSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './thirdSection.css'
+const thirdSection = () => {
+  return (
+    <div id='layer2'>
+        thirdSection
+    </div>
+  )
+}
+
+export default thirdSection
